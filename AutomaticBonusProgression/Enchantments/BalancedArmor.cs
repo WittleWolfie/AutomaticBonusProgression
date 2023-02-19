122f6a6f7d0a838d9d89230c9f3946deaaeafe73
TylerGoeringer <tyler.goeringer@gmail.com>
2023-02-19 14:45:24 -0800

    Convert Balanced Armor to use util function

--- AutomaticBonusProgression/Enchantments/BalancedArmor.cs
+++ AutomaticBonusProgression/Enchantments/BalancedArmor.cs
@@ -1,15 +1,9 @@
 ﻿using AutomaticBonusProgression.Components;
-using AutomaticBonusProgression.Patches;
 using AutomaticBonusProgression.Util;
-using BlueprintCore.Blueprints.Configurators.UnitLogic.ActivatableAbilities;
-using BlueprintCore.Blueprints.CustomConfigurators.Classes;
-using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Buffs;
 using BlueprintCore.Blueprints.References;
 using Kingmaker.Blueprints;
 using Kingmaker.Blueprints.Classes;
 using Kingmaker.Designers.Mechanics.Facts;
-using Kingmaker.UnitLogic.ActivatableAbilities;
-using static Kingmaker.UnitLogic.Commands.Base.UnitCommand;
 
 namespace AutomaticBonusProgression.Enchantments
 {
@@ -22,7 +16,7 @@ namespace AutomaticBonusProgression.Enchantments
     private const string AbilityName = "LegendaryArmor.Balanced.Ability";
 
     private const string DisplayName = "LegendaryArmor.Balanced.Name";
-    private const int Enhancement = 1;
+    private const int EnhancementCost = 1;
 
     internal static BlueprintFeature Configure()
     {
@@ -30,36 +24,22 @@ namespace AutomaticBonusProgression.Enchantments
 
       var balancedFeature =
         EnchantmentTool.AddEnhancementEquivalence(
-          FeatureRefs.ArcaneArmorBalancedFeature, EnhancementType.Armor, Enhancement);
+          FeatureRefs.ArcaneArmorBalancedFeature, EnhancementType.Armor, EnhancementCost);
 
       var enchant = ArmorEnchantmentRefs.ArcaneArmorBalancedEnchant.Reference.Get();
-      var buff = BuffConfigurator.New(BuffName, Guids.BalancedArmorBuff)
-        .SetDisplayName(DisplayName)
-        .SetDescription(enchant.m_Description)
-        //.SetIcon()
-        .AddComponent(balancedFeature.GetComponent<CMDBonusAgainstManeuvers>())
-        .AddComponent(new EnhancementEquivalenceComponent(EnhancementType.Armor, Enhancement))
-        .Configure();
-
-      var ability = ActivatableAbilityConfigurator.New(AbilityName, Guids.BalancedArmorAbility)
-        .SetDisplayName(DisplayName)
-        .SetDescription(enchant.m_Description)
-        //.SetIcon()
-        .SetBuff(buff)
-        .SetDeactivateImmediately()
-        .SetActivationType(AbilityActivationType.Immediately)
-        .SetActivateWithUnitCommand(CommandType.Free)
-        .SetHiddenInUI()
-        .AddComponent(new EnhancementEquivalentRestriction(EnhancementType.Armor, Enhancement))
-        .Configure();
-
-      return FeatureConfigurator.New(BalancedArmorName, Guids.BalancedArmor)
-        .SetIsClassFeature()
-        .SetDisplayName(DisplayName)
-        .SetDescription(enchant.m_Description)
-        //.SetIcon()
-        .AddFacts(new() { ability })
-        .Configure();
+      return EnchantmentTool.CreateEnchant(
+        buffName: BuffName,
+        buffGuid: Guids.FortificationBuff,
+        displayName: DisplayName,
+        description: enchant.m_Description,
+        //icon: ??,
+        type: EnhancementType.Armor,
+        enhancementCost: EnhancementCost,
+        abilityName: AbilityName,
+        abilityGuid: Guids.FortificationAbility,
+        featureName: BalancedArmorName,
+        Guids.BalancedArmor,
+        buffComponents: balancedFeature.GetComponent<CMDBonusAgainstManeuvers>());
     }
   }
 }
