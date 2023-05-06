b3360b11af13808cbcf32420fe5ed51a9007bd86
TylerGoeringer <tyler.goeringer@gmail.com>
2023-05-06 09:31:05 -0700

    Update Prefabs.cs

--- AutomaticBonusProgression/UI/Prefabs.cs
+++ AutomaticBonusProgression/UI/Prefabs.cs
@@ -10,14 +10,15 @@ using TMPro;
 using UnityEngine;
 
 namespace AutomaticBonusProgression.UI
-{/// <summary>
- /// Class storing prefab objects used to instantiate elements and containers.
- /// </summary>
- /// 
- /// <remarks>
- /// Each Prefab is a copy of some original prefab. This allows edit any components on the object once to create
- /// a prefab, rather than using a base game prefab and editing components each time it is used. 
- /// </remarks>
+{
+  /// <summary>
+  /// Class storing prefab objects used to instantiate elements and containers.
+  /// </summary>
+  /// 
+  /// <remarks>
+  /// Each Prefab is a copy of some original prefab. This allows edit any components on the object once to create
+  /// a prefab, rather than using a base game prefab and editing components each time it is used. 
+  /// </remarks>
   internal static class Prefabs
   {
     private static readonly Logging.Logger Logger = Logging.GetLogger(nameof(Prefabs));
