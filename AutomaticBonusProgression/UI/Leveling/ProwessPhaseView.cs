98494fffc16173908ff1e70a129c0e81f21a202a
TylerGoeringer <tyler.goeringer@gmail.com>
2023-05-28 16:47:34 -0700

    Start sketching out Prowess leveling UI

---
+++ AutomaticBonusProgression/UI/Leveling/ProwessPhaseView.cs
@@ -0,0 +1,109 @@
+﻿using AutomaticBonusProgression.Features;
+using AutomaticBonusProgression.Util;
+using HarmonyLib;
+using Kingmaker.UI.MVVM._PCView.CharGen.Phases;
+using Kingmaker.UI.MVVM._PCView.CharGen.Phases.AbilityScores;
+using Kingmaker.UI.MVVM._VM.CharGen.Phases.AbilityScores;
+using Owlcat.Runtime.UI.MVVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace AutomaticBonusProgression.UI.Leveling
+{
+  /// <summary>
+  /// View for selecting Prowess bonuses. Extends the ability selection page of leveling
+  /// </summary>
+  internal class ProwessPhaseView : ViewBase<ProwessPhaseVM>
+  {
+    private static readonly Logging.Logger Logger = Logging.GetLogger(nameof(ProwessPhaseView));
+
+    internal void Initialize()
+    {
+
+    }
+
+    public override void BindViewImplementation()
+    {
+      throw new NotImplementedException();
+    }
+
+    public override void DestroyViewImplementation()
+    {
+      throw new NotImplementedException();
+    }
+
+    #region Setup
+    private static ProwessPhaseView BaseView;
+
+    [HarmonyPatch(typeof(CharGenAbilityScoresDetailedPCView))]
+    internal class CharGenAbilityScoresDetailedPCView_Patch
+    {
+      [HarmonyPatch(nameof(CharGenAbilityScoresDetailedPCView.Initialize)), HarmonyPostfix]
+      static void Initialize(CharGenAbilityScoresDetailedPCView __instance)
+      {
+        try
+        {
+          Logger.Log($"Initializing ProwessPhaseView");
+          BaseView = Create(__instance);
+        }
+        catch (Exception e)
+        {
+          Logger.LogException("CharGenAbilityScoresDetailedPCView_Patch.Initialize", e);
+        }
+      }
+
+      [HarmonyPatch(nameof(CharGenAbilityScoresDetailedPCView.BindViewImplementation)), HarmonyPostfix]
+      static void BindViewImplementation(CharGenAbilityScoresDetailedPCView __instance)
+      {
+        try
+        {
+          Logger.Log($"Binding ProwessPhaseVM");
+          // TODO:
+          // - Hook up data / VM to the selector
+          // - Handle binding, make sure it's all being disposed
+          // - Tweak view to get the right size & position and all that
+          // - Patch CharGenVM.NeedAbilityScoresPhase() when Prowess is needed
+        }
+        catch (Exception e)
+        {
+          Logger.LogException("CharGenAbilityScoresDetailedPCView_Patch.Initialize", e);
+        }
+      }
+
+      internal static ProwessPhaseView Create(CharGenAbilityScoresDetailedPCView abilityScoreView)
+      {
+        var racialBonusSelector = abilityScoreView.gameObject.ChildObject("AllocatorPlace/Selector/RaceBonusSelector");
+        // To create the prowess selection copy the Racial Bonus Selector, then copy the individual selector to create
+        // one for Mental and one for Physical (showing one or both as needed)
+        var obj = GameObject.Instantiate(racialBonusSelector);
+        obj.transform.AddTo(racialBonusSelector.transform.parent);
+
+        // Set up the label
+        var label = obj.gameObject.ChildObject("RedLabel (1)/LabelText").GetComponent<TextMeshProUGUI>();
+        label.SetText(UITool.GetString("Leveling.Prowess"));
+
+        // Create a second selector
+        var additionalSelector = GameObject.Instantiate(obj.gameObject.ChildObject("SqeuntionalSelector"));
+        additionalSelector.transform.AddTo(obj.transform);
+
+        var view = obj.AddComponent<ProwessPhaseView>();
+        view.Initialize();
+        return view;
+      }
+    }
+    #endregion
+  }
+
+  internal class ProwessPhaseVM : BaseDisposable, IViewModel
+  {
+    public override void DisposeImplementation()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
